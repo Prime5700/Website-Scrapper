6163b4685c9c766a1fb514d7e751cc0d01fa3aa0
Prime5700 <84388300+Prime5700@users.noreply.github.com>
2023-04-24 15:24:14 +0530

    Add files via upload

---
+++ index.mjs
@@ -0,0 +1,19 @@
+//const scrape = require('website-scraper');
+import scrape from 'website-scraper';
+const websiteUrl = 'https://testobooster.in/'; 
+ 
+scrape({ 
+    urls: [websiteUrl], 
+    urlFilter: function (url) { 
+        return url.indexOf(websiteUrl) === 0; 
+    }, 
+    recursive: true, 
+    maxDepth: 50, 
+    prettifyUrls: true, 
+    filenameGenerator: 'bySiteStructure', 
+    directory: './node-website' 
+}).then((data) => { 
+    console.log("Entire website succesfully downloaded"); 
+}).catch((err) => { 
+    console.log("An error ocurred", err); 
+}); 
